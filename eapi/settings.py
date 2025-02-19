630f2d69e18fca9efa8949433c7d03d11088f326
prakash <prakash@DESKTOP-C26OAB4.localdomain>
2025-02-19 15:47:56 +0000

    settings.py modified for whitenose

--- eapi/settings.py
+++ eapi/settings.py
@@ -76,6 +76,7 @@ SIMPLE_JWT = {
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
