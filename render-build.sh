81958fd211b335e8d4dc3d0e9b3518bc064bc8ad
prakash <prakash@DESKTOP-C26OAB4.localdomain>
2025-02-20 03:38:29 +0000

    superuser auto generate

--- render-build.sh
+++ render-build.sh
@@ -6,6 +6,5 @@ python manage.py migrate
 echo "Creating Superuser (if not exists)..."
 python manage.py createsuperuser
 
-echo "Collecting Static Files..."
-python manage.py collectstatic --noinput
+
 
