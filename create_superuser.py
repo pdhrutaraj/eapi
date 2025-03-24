a25a63c9422fa71ff3668274371e3064f52c6050
pdhrutaraj <pdhrutaraj@gmail.com>
2025-03-24 15:25:15 +0530

    autocreate superuser

--- create_superuser.py
+++ create_superuser.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
-email = os.environ.get('DJANGO_SUPERUSER_EMAIL')
-password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
+username = os.environ.get('DJANGO_SUPERUSER_USERNAME','admin')
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL','pdhrutaraj@gmail.com')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD','admin')
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
